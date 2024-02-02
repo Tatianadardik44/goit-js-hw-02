6df876c507d4fdec45186d30b152655f9d5d8eb0
Tatianadardik44 <dardiktatiana@gmail.com>
2024-02-02 12:38:47 +0100

    cloning

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,1 @@
-function getElementWidth(content, padding, border) {
-    let contents = Number.parseFloat(content);
-    let paddings = Number.parseFloat(padding);
-    let borders = Number.parseFloat(border);
 
-    const value = contents + paddings * 2 + borders * 2;
-    
-    return value;
-}   
- 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
